54d91a2da44b12c915a3939dbf4d7bb8519c477d
TonyStark-max <somthupakula2@gmail.com>
2026-03-03 00:49:35 +0530

    Testing before controller

---
+++ src/main/java/com/URL/ScaleLink/CLITest/Test.java
@@ -0,0 +1,19 @@
+//package com.URL.ScaleLink.CLITest;
+//
+//import com.URL.ScaleLink.Repository.UrlRepo;
+//import com.URL.ScaleLink.Service.UrlService;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.boot.CommandLineRunner;
+//import org.springframework.stereotype.Component;
+//
+//@Component
+//@RequiredArgsConstructor
+//public class Test implements CommandLineRunner {
+//    private final UrlRepo urlRepo;
+//
+//    @Override
+//    public void run(String... args) throws Exception{
+//
+//        System.out.println("Generated Short Code :"+urlRepo.findAll());
+//    }
+//}
